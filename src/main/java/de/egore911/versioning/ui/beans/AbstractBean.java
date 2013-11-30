3159703c9ad6bea1d326dac74d283d445f4afa9a
Christoph Brill <egore911@gmail.com>
2013-11-30 22:16:58 +0100

    Add mising headers

--- src/main/java/de/egore911/versioning/ui/beans/AbstractBean.java
+++ src/main/java/de/egore911/versioning/ui/beans/AbstractBean.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013  Christoph Brill <egore911@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.egore911.versioning.ui.beans;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +25,9 @@ import de.egore911.versioning.util.SessionUtil;
 import de.egore911.versioning.util.security.PermissionException;
 import de.egore911.versioning.util.security.RequiresPermission;
 
+/**
+ * @author Christoph Brill &lt;egore911@gmail.com&gt;
+ */
 public class AbstractBean {
 
 	@PostConstruct
