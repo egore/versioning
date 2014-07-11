72fc083cd15ad519631f624dba18a57914b730d0
Christoph Brill <egore911@gmail.com>
2014-07-11 14:13:24 +0200

    Implement deletion of VCS hosts from the overview

--- src/main/java/de/egore911/versioning/ui/beans/list/VcshostList.java
+++ src/main/java/de/egore911/versioning/ui/beans/list/VcshostList.java
@@ -16,14 +16,20 @@
  */
 package de.egore911.versioning.ui.beans.list;
 
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import de.egore911.versioning.persistence.dao.VcshostDao;
 import de.egore911.versioning.persistence.model.Permission;
+import de.egore911.versioning.persistence.model.Project;
 import de.egore911.versioning.persistence.model.VcsHost;
 import de.egore911.versioning.persistence.model.VcsHost_;
 import de.egore911.versioning.persistence.selector.VcshostSelector;
+import de.egore911.versioning.util.SessionUtil;
 import de.egore911.versioning.util.security.RequiresPermission;
 
 /**
@@ -46,6 +52,30 @@ public class VcshostList extends AbstractList<VcsHost> {
 		state.setAscending(Boolean.TRUE);
 		state.setLimit(DEFAULT_LIMIT);
 		return state;
+	}
+
+	public void delete(Integer id) {
+		VcshostDao vcshostDao = getDao();
+		VcsHost vcshost = vcshostDao.findById(id);
+		if (!vcshost.getProjects().isEmpty()) {
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ResourceBundle bundle = SessionUtil.getBundle();
+			StringBuilder projectNames = new StringBuilder();
+			for (Project project : vcshost.getProjects()) {
+				if (projectNames.length() > 0) {
+					projectNames.append(", ");
+				}
+				projectNames.append(project.getName());
+			}
+			FacesMessage message = new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					bundle.getString("vcshost_delete_not_possible_used_by_projects"),
+					projectNames.toString());
+			facesContext.addMessage("main:table", message);
+			return;
+		}
+
+		vcshostDao.remove(vcshost);
 	}
 
 }
