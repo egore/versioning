4f202bbcaf58ec5254f60b84451b2b8cb53208e0
Christoph Brill <egore911@gmail.com>
2014-09-14 21:54:00 +0200

    Fix syntax

--- src/main/resources/db/migration/mysql/V1_21__drop_mavenrepository_dependecy_from_project.sql
+++ src/main/resources/db/migration/mysql/V1_21__drop_mavenrepository_dependecy_from_project.sql
@@ -1,4 +1,4 @@
 ALTER TABLE project
   DROP COLUMN id_mavenrepository,
   DROP KEY FK_e50f4sg1vtd5udecjy0b9bo1h,
-  DROP CONSTRAINT FK_e50f4sg1vtd5udecjy0b9bo1h;
+  DROP FOREIGN KEY FK_e50f4sg1vtd5udecjy0b9bo1h;
