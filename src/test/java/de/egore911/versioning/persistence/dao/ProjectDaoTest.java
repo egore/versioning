34be47acd6ba9a63dd6ba91210977a8ea023fe75
Christoph Brill <egore911@gmail.com>
2017-05-14 01:07:02 +0200

    Add unit tests for VcsHostDao and ProjectDao

---
+++ src/test/java/de/egore911/versioning/persistence/dao/ProjectDaoTest.java
@@ -0,0 +1,34 @@
+package de.egore911.versioning.persistence.dao;
+
+import java.util.UUID;
+
+import de.egore911.appframework.testing.AbstractDaoCRUDTest;
+import de.egore911.versioning.persistence.model.ProjectEntity;
+import de.egore911.versioning.persistence.model.VcsHostEntity;
+
+public class ProjectDaoTest extends AbstractDaoCRUDTest<ProjectEntity> {
+
+	@Override
+	protected ProjectEntity createFixture() {
+		VcsHostEntity vcsHost = new VcsHostDaoTest().createFixture();
+		vcsHost = new VcsHostDao().save(vcsHost);
+		
+		ProjectEntity project = new ProjectEntity();
+		project.setName(UUID.randomUUID().toString());
+		project.setDescription(UUID.randomUUID().toString());
+		project.setVcsPath(UUID.randomUUID().toString());
+		project.setVcsHost(vcsHost);
+		return project;
+	}
+
+	@Override
+	protected void modifyFixture(ProjectEntity project) {
+		project.setDescription(UUID.randomUUID().toString());
+	}
+
+	@Override
+	protected ProjectDao getDao() {
+		return new ProjectDao();
+	}
+
+}
---
+++ src/test/java/de/egore911/versioning/persistence/dao/VcsHostDaoTest.java
@@ -0,0 +1,30 @@
+package de.egore911.versioning.persistence.dao;
+
+import java.util.UUID;
+
+import de.egore911.appframework.testing.AbstractDaoCRUDTest;
+import de.egore911.versioning.persistence.model.Vcs;
+import de.egore911.versioning.persistence.model.VcsHostEntity;
+
+public class VcsHostDaoTest extends AbstractDaoCRUDTest<VcsHostEntity> {
+
+	@Override
+	protected VcsHostEntity createFixture() {
+		VcsHostEntity vcsHost = new VcsHostEntity();
+		vcsHost.setName(UUID.randomUUID().toString());
+		vcsHost.setUri(UUID.randomUUID().toString());
+		vcsHost.setVcs(Vcs.git);
+		return vcsHost;
+	}
+
+	@Override
+	protected void modifyFixture(VcsHostEntity vcsHost) {
+		vcsHost.setUri(UUID.randomUUID().toString());
+	}
+
+	@Override
+	protected VcsHostDao getDao() {
+		return new VcsHostDao();
+	}
+
+}
