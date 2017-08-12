6c5cb0254d50462cf995fbaddb48aebfa8562c56
Christoph Brill <egore911@gmail.com>
2017-08-12 13:43:40 +0200

    Add test for tagExists

--- src/test/java/de/egore911/versioning/persistence/dao/VersionDaoTest.java
+++ src/test/java/de/egore911/versioning/persistence/dao/VersionDaoTest.java
@@ -3,8 +3,13 @@ package de.egore911.versioning.persistence.dao;
 import de.egore911.appframework.testing.persistence.dao.AbstractDaoCRUDTest;
 import de.egore911.versioning.persistence.model.ProjectEntity;
 import de.egore911.versioning.persistence.model.VersionEntity;
+import org.junit.Test;
 
 import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 public class VersionDaoTest extends AbstractDaoCRUDTest<VersionEntity> {
 
@@ -29,4 +34,17 @@ public class VersionDaoTest extends AbstractDaoCRUDTest<VersionEntity> {
         return new VersionDao();
     }
 
+    @Test
+    public void testTagExists() {
+
+        ProjectEntity someSvnProject = new ProjectDao().findById(1);
+        assertThat(someSvnProject, notNullValue());
+
+        assertThat(getDao().tagExists(someSvnProject, "1.0.0"), equalTo(true));
+        assertThat(getDao().tagExists(someSvnProject, "1.0.1"), equalTo(true));
+
+        assertThat(getDao().tagExists(someSvnProject, "1.0.0."), equalTo(false));
+        assertThat(getDao().tagExists(someSvnProject, "1.0.1.0"), equalTo(false));
+        assertThat(getDao().tagExists(someSvnProject, "3.0.0"), equalTo(false));
+    }
 }
