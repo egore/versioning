71857ba8e27979f41e364bce85ead7fc03797aae
Christoph Brill <egore911@gmail.com>
2018-03-12 13:53:52 +0100

    Add missing properties to maven artifact

--- src/main/java/de/egore911/versioning/ui/dto/MavenArtifact.java
+++ src/main/java/de/egore911/versioning/ui/dto/MavenArtifact.java
@@ -4,4 +4,32 @@ import de.egore911.appframework.ui.dto.AbstractDto;
 
 public class MavenArtifact extends AbstractDto {
 
+	private String groupId;
+	private String artifactId;
+	private String packaging;
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
+
+	public String getPackaging() {
+		return packaging;
+	}
+
+	public void setPackaging(String packaging) {
+		this.packaging = packaging;
+	}
+
 }
